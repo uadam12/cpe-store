3dbdfa26f1683242ca190f549ed4f6e529ee175a
Usman Adam Imam <uadam12@gmail.com>
2026-03-04 13:27:32 +0100

    Use relative path for products images

--- scripts/products.js
+++ scripts/products.js
@@ -4,55 +4,55 @@ export const products = [
         name: "Arduino Uno R3",
         price: 28000,
         category: "Microcontrollers",
-        image: "/assets/arduino.jpg"
+        image: "assets/arduino.jpg"
     },
     {
         id: 2,
         name: "ESP32 WiFi + Bluetooth Module",
         price: 18500,
         category: "IoT Boards",
-        image: "/assets/esp32.webp"
+        image: "assets/esp32.webp"
     },
     {
         id: 3,
         name: "16x2 LCD Display (I2C)",
         price: 6500,
         category: "Displays",
-        image: "/assets/display.png"
+        image: "assets/display.png"
     },
     {
         id: 4,
         name: "Ultrasonic Distance Sensor (HC-SR04)",
         price: 4500,
         category: "Sensors",
-        image: "/assets/Ultrasonic.jpg"
+        image: "assets/Ultrasonic.jpg"
     },
     {
         id: 5,
         name: "Soil Moisture Sensor",
         price: 3500,
         category: "Sensors",
-        image: "/assets/moisture.webp"
+        image: "assets/moisture.webp"
     },
     {
         id: 6,
         name: "Relay Module (5V, 2-Channel)",
         price: 7200,
         category: "Control Modules",
-        image: "/assets/relay.jpg"
+        image: "assets/relay.jpg"
     },
     {
         id: 7,
         name: "Breadboard + Jumper Wire Kit",
         price: 9500,
         category: "Prototyping",
-        image: "/assets/breedboard.jpg"
+        image: "assets/breedboard.jpg"
     },
     {
         id: 8,
         name: "Motor Driver Module",
         price: 3800,
         category: "Mechanical",
-        image: "/assets/motor-driver.jpg"
+        image: "assets/motor-driver.jpg"
     }
 ];
